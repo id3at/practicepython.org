c6d3a6f36e45b567c8e55f465b898605913016af
id3at <58143208+id3at@users.noreply.github.com>
2020-01-16 14:45:27 +0100

    Add files via upload

---
+++ GuessLetters.py
@@ -0,0 +1,33 @@
+"""
+Autor id3at
+Program, losuje slowo z pliku, "sowpods".
+Uzytkownik zgaduje slowo za pomoca wskazywania liter..
+"""
+
+import random
+
+with open("sowpods.txt", "r") as txt:
+    c = txt.readlines()
+slow = {e: line.strip() for e, line in enumerate(c)}
+slowo = slow[random.randint(0, 267750)]
+
+
+tex = "_"* len(slowo)
+zgad = list(tex)
+slli = list(slowo)
+loss = slli[:]
+
+print(slli)
+print(zgad)
+
+while zgad != slli:
+    litera = input("zgadnij litere")
+    litera = litera.upper()
+    if  litera in loss:
+        while litera in loss:
+            zgad[loss.index(litera)] = litera
+            loss[loss.index(litera)] = "."
+        print(zgad)
+    else:
+        print("Nie ma takiej litery w słowie")
+        print(zgad)
