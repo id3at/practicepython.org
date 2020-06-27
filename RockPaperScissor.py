10f89437b6095f9a5a0d2b23d14ec600972f9f6b
Tomasz <58143208+id3at@users.noreply.github.com>
2020-06-27 19:20:54 +0200

    Update RockPaperScissor.py

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -2,9 +2,18 @@
 Autor Tomasz Głuc
 Game Rock Paper and Scissors
 """
-import random 
+import random
 import os
-gra = {('Rock', 'Rock'): "Draw", ('Rock', 'Paper'): "Paper Win!", ('Rock', 'Scissors'): "Rock Win!", ('Paper', 'Rock'): "Paper Win!", ('Paper', 'Paper'): "Draw", ('Paper', 'Scissors'): "Scissors Win!", ('Scissors', 'Rock'): "Rock Win!", ('Scissors', 'Paper'): "Scissors Win!", ('Scissors', 'Scissors'): "Draw"}
+gra = {('Rock', 'Rock'): "Draw",
+       ('Rock', 'Paper'): "Paper Win!",
+       ('Rock', 'Scissors'): "Rock Win!",
+       ('Paper', 'Rock'): "Paper Win!",
+       ('Paper', 'Paper'): "Draw",
+       ('Paper', 'Scissors'): "Scissors Win!",
+       ('Scissors', 'Rock'): "Rock Win!",
+       ('Scissors', 'Paper'): "Scissors Win!",
+       ('Scissors', 'Scissors'): "Draw"}
+
 
 petla = "y"
 
@@ -12,24 +21,22 @@ while petla == "y":
     try:
         elementy = ["Rock", "Paper", "Scissors"]
         komp = random.choice(elementy)
-        
         print("Game: Rock, Scissors and Paper")
-        for t, e in enumerate(elementy):
-            print(t, "=", e)
+        for enum, elem in enumerate(elementy):
+            print(enum, "=", elem)
 
         uzytkownik = elementy[int(input("You Chooice: "))]
         dana = (uzytkownik, komp)
-        
-        for t in gra:
-            if t == dana:
+        for klucz in gra:
+            if klucz == dana:
                 print(f"You Chooise: {uzytkownik}.\nComputer Chooise: {komp}")
-                if komp in gra[t]:
+                if komp in gra[klucz]:
                     print('Computer Win')
-                elif uzytkownik in gra[t]:
+                elif uzytkownik in gra[klucz]:
                     print('You Win!')
                 else:
-                    print(gra[t], )
+                    print(gra[klucz])
         petla = input("If you want to continue press: y, if not, press: n")
         os.system('cls')
-    except:
+    except ValueError:
         print("Select number!")
